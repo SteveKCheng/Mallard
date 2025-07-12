127e92f38fd25c3c696a15bd11780a32a57c4fad
Steve Cheng <steve.ckp@gmail.com>
2025-07-11 23:30:17 -0400

    Add unit test project

---
+++ DuckDB.NET.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DuckDB.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        using var dbConn = new DuckDbConnection(@"test.db");
+        return;
+    }
+
+}
