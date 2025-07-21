579a88bbf2afb61ddbe1bde9441387d116277e51
Steve Cheng <steve.ckp@gmail.com>
2025-07-21 13:52:43 -0400

    Move boxing wrapper for VectorElementConverter into separate source file

---
+++ Mallard/Vector/VectorElementConverter.Wrappers.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace Mallard;
+
+internal readonly partial struct VectorElementConverter
+{
+    #region Boxing converters
+
+    private static VectorElementConverter
+        CreateForBoxedType(in DuckDbVectorInfo vector)
+    {
+        var converter = vector.BasicType switch
+        {
+            DuckDbBasicType.Boolean => CreateForBoxedPrimitive<bool>(),
+
+            DuckDbBasicType.TinyInt => CreateForBoxedPrimitive<sbyte>(),
+            DuckDbBasicType.SmallInt => CreateForBoxedPrimitive<short>(),
+            DuckDbBasicType.Integer => CreateForBoxedPrimitive<int>(),
+            DuckDbBasicType.BigInt => CreateForBoxedPrimitive<long>(),
+
+            DuckDbBasicType.UTinyInt => CreateForBoxedPrimitive<byte>(),
+            DuckDbBasicType.USmallInt => CreateForBoxedPrimitive<ushort>(),
+            DuckDbBasicType.UInteger => CreateForBoxedPrimitive<uint>(),
+            DuckDbBasicType.UBigInt => CreateForBoxedPrimitive<ulong>(),
+
+            DuckDbBasicType.Float => CreateForBoxedPrimitive<float>(),
+            DuckDbBasicType.Double => CreateForBoxedPrimitive<double>(),
+
+            DuckDbBasicType.Date => CreateForBoxedPrimitive<DuckDbDate>(),
+            DuckDbBasicType.Timestamp => CreateForBoxedPrimitive<DuckDbTimestamp>(),
+
+            DuckDbBasicType.Interval => CreateForBoxedPrimitive<DuckDbInterval>(),
+
+            DuckDbBasicType.UHugeInt => CreateForBoxedPrimitive<UInt128>(),
+            DuckDbBasicType.HugeInt => CreateForBoxedPrimitive<Int128>(),
+
+            DuckDbBasicType.Decimal => DuckDbDecimal.GetBoxedVectorElementConverter(vector),
+
+            _ => default
+        };
+
+        // Above primitives have efficient implementations where the casting
+        // is inlined into the conversion function.
+        if (converter.IsValid)
+            return converter;
+
+        // Decide on what the original (unboxed) type first.
+        converter = CreateForType(null, vector);
+
+        // Nothing available, or the target type is a reference type so no boxing needed.
+        if (!converter.IsValid || !converter.TargetType.IsValueType)
+            return converter;
+
+        // Set up a second indirect call to box the return value 
+        // from the original converter.
+        return UnsafeCreateFromGeneric(CreateBoxingWrapperMethod,
+                                       converter,
+                                       vector,
+                                       converter.TargetType);
+    }
+
+    private static readonly MethodInfo CreateBoxingWrapperMethod =
+    typeof(BoxingConverter).GetMethod(nameof(BoxingConverter.Create),
+                                      BindingFlags.Static | BindingFlags.NonPublic)!;
+
+    /// <summary>
+    /// Boxes the results of a vector element conversion that returns a value type, 
+    /// when the client requests such.
+    /// </summary>
+    /// <remarks>
+    /// Used internally by <see cref="VectorElementConverter.CreateForBoxedPrimitive{T}" />
+    /// when no more efficient alternative is available.
+    /// </remarks>
+    internal sealed class BoxingConverter
+    {
+        private readonly VectorElementConverter _unboxedConverter;
+        private BoxingConverter(VectorElementConverter unboxedConverter)
+            => _unboxedConverter = unboxedConverter;
+        private static object Convert<T>
+            (BoxingConverter self, in DuckDbVectorInfo vector, int index) where T : struct
+            => (object)self._unboxedConverter.Invoke<T>(vector, index, requireValid: true);
+
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+        internal static unsafe VectorElementConverter Create<T>
+            (VectorElementConverter unboxedConverter, in DuckDbVectorInfo _) where T : struct
+            => VectorElementConverter.Create(new BoxingConverter(unboxedConverter), &Convert<T>);
+    }
+
+    #endregion
+}
