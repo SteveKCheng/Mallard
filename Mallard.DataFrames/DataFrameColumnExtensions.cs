f87cf1b302894aa678aa798d1b82e4bfeb0c7fef
Steve Cheng <steve.ckp@gmail.com>
2025-07-27 15:34:58 -0400

    Sketch of what integration with Microsoft.Data.Analysis.DataFrame would look

---
+++ Mallard.DataFrames/DataFrameColumnExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.Analysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mallard.DataFrames;
+
+public static class DataFrameColumnExtensions
+{
+    public static void AppendFrom<T>(this PrimitiveDataFrameColumn<T> column, in DuckDbVectorRawReader<T> reader) 
+        where T : unmanaged
+    {
+        for (int i = 0; i < reader.Length; ++i)
+        {
+            bool valid = reader.TryGetItem(i, out var value);
+            column.Append(valid ? value : null);
+        }
+    }
+
+    public static void AppendFrom<T>(this PrimitiveDataFrameColumn<T> column, in DuckDbVectorReader<T> reader) 
+        where T : unmanaged
+    {
+        for (int i = 0; i < reader.Length; ++i)
+        {
+            bool valid = reader.TryGetItem(i, out var value);
+            column.Append(valid ? value : null);
+        }
+    }
+
+    public static void AppendFrom(this StringDataFrameColumn column, in DuckDbVectorRawReader<DuckDbString> reader)
+    {
+        for (int i = 0; i < reader.Length; ++i)
+        {
+            bool valid = reader.TryGetItem(i, out var value);
+            column.Append(valid ? value.ToString() : null);
+        }
+    }
+}
---
+++ Mallard.DataFrames/DataFrameExtensions.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.Analysis;
+using System;
+using System.Linq;
+
+namespace Mallard.DataFrames;
+using DataFrameColumnAndReader = (DataFrameColumn Output, Action<DataFrameColumn, DuckDbChunkReader, int> ReadAction);
+   
+public static class DataFrameExtensions
+{
+    public static DataFrame Create(DuckDbResult queryResult)
+    {
+        var columns = new DataFrameColumnAndReader[queryResult.ColumnCount];
+
+        for (int columnIndex = 0; columnIndex < columns.Length; ++columnIndex)
+        {
+            var columnInfo = queryResult.GetColumnInfo(columnIndex);
+            var columnName = queryResult.GetColumnName(columnIndex);
+            columns[columnIndex] = columnInfo.ValueKind switch
+            {
+                DuckDbValueKind.Boolean => GetColumnAndReaderRaw<bool>(columnName),
+                DuckDbValueKind.TinyInt => GetColumnAndReaderRaw<sbyte>(columnName),
+                DuckDbValueKind.SmallInt => GetColumnAndReaderRaw<short>(columnName),
+                DuckDbValueKind.Integer => GetColumnAndReaderRaw<int>(columnName),
+                DuckDbValueKind.BigInt => GetColumnAndReaderRaw<long>(columnName),
+
+                DuckDbValueKind.UTinyInt => GetColumnAndReaderRaw<byte>(columnName),
+                DuckDbValueKind.USmallInt => GetColumnAndReaderRaw<ushort>(columnName),
+                DuckDbValueKind.UInteger => GetColumnAndReaderRaw<uint>(columnName),
+                DuckDbValueKind.UBigInt => GetColumnAndReaderRaw<ulong>(columnName),
+
+                DuckDbValueKind.Float => GetColumnAndReaderRaw<float>(columnName),
+                DuckDbValueKind.Double => GetColumnAndReaderRaw<double>(columnName),
+
+                DuckDbValueKind.VarChar => GetStringColumnAndReader(columnName),
+
+                DuckDbValueKind.Decimal => GetColumnAndReader<decimal>(columnName),
+
+                _ => throw new NotSupportedException()
+            };
+        }
+
+        queryResult.ProcessAllChunks(columns, static (in DuckDbChunkReader reader, DataFrameColumnAndReader[] columns) =>
+        {
+            for (int columnIndex = 0; columnIndex < columns.Length; ++columnIndex)
+            {
+                var (output, readAction) = columns[columnIndex];
+                readAction(output, reader, columnIndex);
+            }
+            return false;   // unused
+        });
+
+        return new DataFrame(columns.Select(c => c.Output));
+    }
+
+    private static DataFrameColumnAndReader GetColumnAndReaderRaw<T>(string name) where T : unmanaged
+    {
+        static void Read(DataFrameColumn target, DuckDbChunkReader reader, int columnIndex)
+                            => ((PrimitiveDataFrameColumn<T>)target).AppendFrom(reader.GetColumnRaw<T>(columnIndex));
+        return (new PrimitiveDataFrameColumn<T>(name), Read);
+    }
+
+    public static DataFrameColumnAndReader GetStringColumnAndReader(string name)
+    {
+        static void Read(DataFrameColumn target, DuckDbChunkReader reader, int columnIndex)
+                            => ((StringDataFrameColumn)target).AppendFrom(reader.GetColumnRaw<DuckDbString>(columnIndex));
+        return (new StringDataFrameColumn(name), Read);
+    }
+
+    private static DataFrameColumnAndReader GetColumnAndReader<T>(string name) where T : unmanaged
+    {
+        static void Read(DataFrameColumn target, DuckDbChunkReader reader, int columnIndex)
+                            => ((PrimitiveDataFrameColumn<T>)target).AppendFrom(reader.GetColumn<T>(columnIndex));
+        return (new PrimitiveDataFrameColumn<T>(name), Read);
+    }
+}
