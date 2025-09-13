aa3482f76ca34c1c11794a9c71321c6c1815e8ec
Steve Cheng <steve.ckp@gmail.com>
2025-09-12 22:30:08 -0400

    Add comments on IDbConnection-related variables and fix warning

--- Mallard/Common/DuckDbDatabase.IDbConnection.cs
+++ Mallard/Common/DuckDbDatabase.IDbConnection.cs
@@ -1,26 +1,53 @@
 ﻿using System;
 using System.Data;
 using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Mallard;
 
 public sealed partial class DuckDbConnection : IDbConnection
 {
+    /// <summary>
+    /// Cached connection string returned by <see cref="IDbConnection.ConnectionString" />.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This class, and DuckDB, do not use the .NET-/ODBC-style connection string to open 
+    /// databases, yet constructing the string is relatively expensive, so it is cached
+    /// on the first query (which might not happen at all).
+    /// </para>
+    /// <para>
+    /// If the user sets the connection string, then this variables contains that
+    /// setting.  Its value will then be used when <see cref="IDbConnection.Open" />
+    /// is called.
+    /// </para>
+    /// </remarks>
     private string? _connectionString;
+    
+    /// <summary>
+    /// Set to true if the user changed <see cref="IDbConnection.ConnectionString" />.
+    /// </summary>
+    /// <remarks>
+    /// If true, the connection string needs to be parsed when
+    /// <see cref="IDbConnection.Open" /> is called.
+    /// </remarks>
     private bool _connectionStringChanged;
 
-    string? IDbConnection.ConnectionString
+    [AllowNull] // This attribute appears in the IDbConnection interface:
+                // "get" should not return null but "set" accepts null for this property.
+    string IDbConnection.ConnectionString
     {
         get
         {
             if (_connectionString != null)
                 return _connectionString;
-            
+
+            var database = _database;
             var builder = new DbConnectionStringBuilder(useOdbcRules: true);
-            builder.Add("path", _database.Path);
-            if (!_database.Options.IsDefault)
+            builder.Add("path", database.Path);
+            if (!database.Options.IsDefault)
             {
-                foreach (var (key, value) in _database.Options)
+                foreach (var (key, value) in database.Options)
                     builder.Add(key, value);
             }
 
@@ -29,7 +56,7 @@ public sealed partial class DuckDbConnection : IDbConnection
         }
         set
         {
-            _connectionString = value;
+            _connectionString = value ?? string.Empty;
             _connectionStringChanged = true;
         }
     }
