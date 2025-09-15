772189fc174726bc12a971de52957b7357d26e49
Steve Cheng <steve.ckp@gmail.com>
2025-09-15 13:41:46 -0400

    Re-design DuckDbParameterCollection to no longer cache name look-ups

--- Mallard/Ado/DuckDbParameterCollection.cs
+++ Mallard/Ado/DuckDbParameterCollection.cs
@@ -4,210 +4,189 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Mallard;
 
-public sealed class DuckDbParameterCollection : IDataParameterCollection, IEnumerable<DbParameter>
+/// <summary>
+/// ADO.NET-compatible collection of parameters for <see cref="DuckDbCommand" />. 
+/// </summary>
+/// <remarks>
+/// <para>
+/// This implementation of <see cref="IDataParameterCollection" /> is essentially a plain list
+/// of the parameters.  Most validation happens when the <see cref="DuckDbCommand" />
+/// is executed, not when a parameter is added to this collection.
+/// </para>
+/// <para>
+/// It is not recommended to mix positional parameters with named parameters.
+/// DuckDB may assign indices to parameters differently than your code.
+/// </para>
+/// </remarks>
+public sealed class DuckDbParameterCollection : IDataParameterCollection, IList<DbParameter>
 {
-    private List<DbParameter> _items = new List<DbParameter>();
-    private Dictionary<string, int> _nameMap = new Dictionary<string, int>();
+    /// <summary>
+    /// Parameters accessed by index.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Because <see cref="IDbDataParameter.ParameterName" /> is a mutable property that
+    /// can be changed at any time by the user (a bad design from the "enterprisey" code
+    /// patterns from the beginning of the millenium)
+    /// it is hard to cache the parameters keyed by name.  In
+    /// other implementations, such as <see cref="System.Data.Odbc.OdbcParameterCollection" />
+    /// and <see cref="Microsoft.Data.SqlClient.SqlParameterCollection" />, when the user
+    /// asks to look up a parameter by name, linear search is performed.  We follow their
+    /// precedent.  It is unlikely that client code using ADO.NET depends on the name
+    /// look-up being O(1).
+    /// </para>
+    /// </remarks>
+    private readonly List<DbParameter> _items = new List<DbParameter>();
 
     IEnumerator<DbParameter> IEnumerable<DbParameter>.GetEnumerator() => _items.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => _items.GetEnumerator();
 
-    public void CopyTo(Array array, int index)
-    {
-        ArgumentOutOfRangeException.ThrowIfNegative(index, "index");
-        
-        if (_items.Count > array.Length - index)
-        {
-            throw new ArgumentException(
-                "The number of elements in this collection is greater than " +
-                "the number of elements from the specified index to the end of the destination array.");
-        }
+    public void CopyTo(Array array, int index) => ((IList)_items).CopyTo(array, index);
 
-        for (int j = 0; j < _items.Count; ++j)
-            array.SetValue(_items[j], index + j);
-    }
+    public bool Remove(DbParameter item) => _items.Remove(item);
 
     public int Count => _items.Count;
     public bool IsSynchronized => false;
     public object SyncRoot => _items;
 
-    public int Add(DbParameter value) => Add(value.ParameterName, value);
-
-    private int Add(string parameterName, DbParameter value)
-    {
-        int index = _items.Count;
-
-        if (parameterName != value.ParameterName)
-        {
-            throw new ArgumentException(
-                "The value of the ParameterName property does not match the name that the parameter is to be added under. "
-                + $" Key: {parameterName}; ParameterName: {value.ParameterName}");
-        }
-
-        if (!_nameMap.TryAdd(parameterName, index))
-        {
-            throw new ArgumentException(
-                $"A parameter with the same name already exists in the same collection. Key: {parameterName}");
-        }
-        
-        _items.Add(value);
-
-        return index;
-    }
+    public void Add(DbParameter value) => _items.Add(ValidateParameter(value));
 
     int IList.Add(object? value)
     {
-        ArgumentNullException.ThrowIfNull(value);
-        return Add((DbParameter)value);
+        var index = _items.Count;
+        _items.Add(ValidateParameter(value));
+        return index;
     }
 
-    public void Clear()
-    {
-        _items.Clear();
-        _nameMap.Clear();
-    }
+    public void Clear() => _items.Clear();
 
-    public bool Contains(DbParameter value)
-    {
-        int index = IndexOf(value.ParameterName);
-        return index >= 0 && _items[index] == value;
-    }
+    public bool Contains(DbParameter value) => _items.Contains(value);
     
-    bool IList.Contains(object? value)
+    public void CopyTo(DbParameter[] array, int arrayIndex) => _items.CopyTo(array, arrayIndex);
+
+    bool IList.Contains(object? value) => value is DbParameter p && Contains(p);
+
+    public int IndexOf(DbParameter value) => _items.IndexOf(value);
+
+    public void Insert(int index, DbParameter item) => _items.Insert(index, ValidateParameter(item));
+
+    int IList.IndexOf(object? value) => value is DbParameter p ? IndexOf(p) : -1; 
+
+    void IList.Insert(int index, object? value) => _items.Insert(index, ValidateParameter(value));
+
+    void IList.Remove(object? value)
     {
-        ArgumentNullException.ThrowIfNull(value);
-        return Contains((DbParameter)value);
+        if (value is DbParameter p)
+            _items.Remove(p);
     }
 
-    public int IndexOf(DbParameter value)
-    {
-        int index = IndexOf(value.ParameterName);
-        return index >= 0 && _items[index] == value ? index : -1;
-    }
-
-    int IList.IndexOf(object? value)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-        return IndexOf((DbParameter)value);
-    }
-
-    public void Insert(int index, object? value)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Remove(object? value)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void RemoveAt(int index)
-    {
-        throw new NotImplementedException();
-    }
+    public void RemoveAt(int index) => _items.RemoveAt(index);
 
     public bool IsFixedSize => false;
     public bool IsReadOnly => false;
 
+    private DbParameter ValidateParameter(DbParameter item, [CallerArgumentExpression(nameof(item))] string? paramName = null)
+    {
+        ArgumentNullException.ThrowIfNull(item, paramName);
+        return item;
+    }
+
+    private DbParameter ValidateParameter(object? item,
+                                          [CallerArgumentExpression(nameof(item))] string? paramName = null)
+    {
+        ArgumentNullException.ThrowIfNull(item, paramName);
+        if (item is not DbParameter p)
+            throw new ArgumentException($"Cannot insert anything but a {nameof(DbParameter)} object into DuckDbParameterCollection. ");
+
+        return p;
+    }
+
     public DbParameter this[int index]
     {
         get => _items[index];
-        set
-        {
-            ArgumentOutOfRangeException.ThrowIfLessThan(index, 0);
-            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, _items.Count);
-
-            var newParameterName = value.ParameterName;
-            
-            // The parameter has a new name not seen before.  It replaces the old name,
-            // from the old DbParameter instance at the same index.
-            if (!_nameMap.TryGetValue(newParameterName, out int oldIndex))
-            {
-                var oldParameterName = _items[index].ParameterName;
-                
-                // Need to linearly scan if the user changed the parameter name out from under us
-                if (_nameMap.GetValueOrDefault(oldParameterName, -1) != index)
-                    oldParameterName = _nameMap.Single(p => p.Value == index).Key;
-                
-                _nameMap.Add(newParameterName, index);
-                _nameMap.Remove(oldParameterName);
-            }
-            
-            // The parameter has been renamed to the same name as another parameter
-            // at a different index, which is not allowed.
-            else if (oldIndex != index)
-            {
-                throw new ArgumentException("A parameter with the same name already exists at a different index. ");
-            }
-
-            _items[index] = value;
-        }
+        set => _items[index] = ValidateParameter(value);
     }
+
+    private static bool IsNamedParameter(string parameterName)
+        => !string.IsNullOrEmpty(parameterName);
 
     object? IList.this[int index]
     {
         get => _items[index];
-        set
-        {
-            ArgumentNullException.ThrowIfNull(value);
-            this[index] = (DbParameter)value;
-        }
+        set => _items[index] = ValidateParameter(value);
     }
 
     public bool Contains(string parameterName)
-        => _nameMap.ContainsKey(parameterName);
+        => IndexOf(parameterName) >= 0;
 
     public int IndexOf(string parameterName)
-        => _nameMap.GetValueOrDefault(parameterName, -1);
+        => GetIndexForParameterName(parameterName, false);
 
     public void RemoveAt(string parameterName)
-    {
-        if (!_nameMap.Remove(parameterName, out int index))
-        {
-            throw new ArgumentException(
-                $"A parameter by the given name does not exist in this collection.  Key: {parameterName}");
-        }
-        
-        _items.RemoveAt(index);
-        
-        // Update indices of parameters that occur after the removed parameter
-        foreach (var key in _nameMap.Keys)
-        {
-            ref int value = ref CollectionsMarshal.GetValueRefOrNullRef(_nameMap, key);
-            if (value > index) --value;
-        }
-    }
+        => _items.RemoveAt(GetIndexForParameterName(parameterName));
 
-    object IDataParameterCollection.this[string parameterName]
+    private int GetIndexForParameterName(string parameterName, bool throwIfNotFound = true)
     {
-        get => this[parameterName];
-        set => this[parameterName] = (DbParameter)value;
+        for (int i = 0; i < _items.Count; ++i)
+        {
+            if (_items[i].ParameterName == parameterName)
+                return i;
+        }
+
+        if (throwIfNotFound)
+        {
+            throw new KeyNotFoundException(
+                $"There is no parameter with the given name in this collection. Key: {parameterName}");
+        }
+        
+        return -1;
     }
 
     /// <summary>
     /// Set (the value of) a parameter keyed by its name.
     /// </summary>
     /// <param name="parameterName">
-    /// The name of the parameter to set.
+    /// The name of the parameter to set.  A parameter object with this name must have been
+    /// added earlier to this collection.
     /// </param>
+    /// <remarks>
+    /// <para>
+    /// A named parameter has an index no different than unnamed parameters.  Calling the
+    /// setter on this property will replace the parameter object at the index
+    /// where a parameter with the given name lives.  The replacement parameter object
+    /// may have a different name.  (This behavior is consistent with other implementations
+    /// of <see cref="IDataParameterCollection" />.)
+    /// </para>
+    /// <para>
+    /// If the new parameter object being set is unnamed, its name will automatically be
+    /// set to <paramref name="parameterName" />.
+    /// </para>
+    /// </remarks>
     public DbParameter this[string parameterName]
     {
-        get
-        {
-            int index = _nameMap[parameterName];
-            return _items[index];
-        }
+        get => _items[GetIndexForParameterName(parameterName)];
         set
         {
-            if (_nameMap.TryGetValue(parameterName, out int index))
-                this[index] = value;
-            else
-                Add(parameterName, value);
+            var index = GetIndexForParameterName(parameterName); 
+            ValidateParameter(value);
+            
+            var n = value.ParameterName;
+            if (string.IsNullOrEmpty(n))
+                value.ParameterName = parameterName;
+            
+            _items[index] = value;
         }
+    }
+
+    object IDataParameterCollection.this[string parameterName]
+    {
+        get => this[parameterName];
+        set => this[parameterName] = ValidateParameter(value);
     }
 }
