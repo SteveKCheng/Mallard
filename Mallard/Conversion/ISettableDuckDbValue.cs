661046340aff68c7dd6dbb1fc59e212bf15630b9
Steve Cheng <steve.ckp@gmail.com>
2025-09-21 09:01:10 -0400

    Edit prose in API documentation for ISettableDuckDbValue

--- Mallard/Conversion/ISettableDuckDbValue.cs
+++ Mallard/Conversion/ISettableDuckDbValue.cs
@@ -9,25 +9,25 @@ namespace Mallard;
 /// <para>
 /// There are a several kinds of objects in DuckDB that take in values that may be
 /// of any of the types supported in its SQL dialect, including but not limited to
-/// integers, date/time, strings, and blobs.  Ultimately there needs to be conversions
-/// from .NET values to their native representations under DuckDB.  (This conversion
-/// is in the opposite direction of the conversion that happens when retrieving values/results
+/// integers, date/time, strings, and blobs.  Ultimately there need to be conversions
+/// from .NET values to their native representations under DuckDB.  (This conversions
+/// are in the opposite direction of those that happen when retrieving values/results
 /// from DuckDB.)
 /// </para>
 /// <para>
-/// To facilitate discovery of the .NET types that are supported for conversion
-/// (into DuckDB), one method to set a value, for each supported data type,
-/// is defined in <see cref="DuckDbValue" /> as an extension method.  Thus the static
-/// type is visible statically (instead of being erased as <see cref="object" />
-/// or hidden behind a unconstrained generic parameter).  The compiler can apply
-/// implicit conversions for the supported data types. 
+/// To make the supported types easy to discover, for each supported type,
+/// an extension method to set its values is defined in <see cref="DuckDbValue" />.
+/// Thus, the static type is visible statically, instead of being erased as
+/// <see cref="object" /> or hidden behind a unconstrained generic parameter
+/// (although such methods are also available for the benefit of generic code).
+/// The compiler can also apply implicit conversions for the supported data types. 
 /// </para>
 /// <para>
 /// The receiver object implements this interface, which has no public methods.
 /// All the value-setting operations are implemented in <see cref="DuckDbValue" />.
-/// This simplifies the implementation, and at the same time, allows the user to
-/// define conversions from other data types on almost the same footing as built-in types
-/// supported by Mallard. 
+/// This simplifies the implementation, makes the basic API stable, and
+/// at the same time, allows users to define their own conversions for other data types
+/// on almost the same footing.
 /// </para>
 /// </remarks>
 public interface ISettableDuckDbValue
